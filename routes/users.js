7ddab4279fe415046d23ebabf7cda08619eeadd7
Elena Liubimova <elena.liubimova3@gmail.com>
2023-04-03 21:09:06 +0300

    fix: validation

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/', getUsers);
 
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24),
+    userId: Joi.string().required(),
   }),
 }), getUser);
 
